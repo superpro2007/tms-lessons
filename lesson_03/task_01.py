0a87307572021c939e9d5c7ecc9ecb10cfcd78ec
Yauhen Zhur <evgeni1897@gmail.com>
2023-01-29 16:50:37 +0300

    add task

---
+++ lesson_03/task_01.py
@@ -0,0 +1,9 @@
+name = input('What is your name?\n')
+birth_year = input('what is your year of birth?\n')
+age = 2023 - int(birth_year)
+print('Hello ' + name)
+print('Your age is ' + str(age))
+
+
+
+
