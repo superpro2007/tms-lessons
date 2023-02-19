b71de11d4366c7e54dcc66316240c7dfa48230aa
Yauhen Zhur <evgeni1897@gmail.com>
2023-02-19 13:42:30 +0300

    fix task5

--- homework_09/university.py
+++ homework_09/university.py
@@ -7,17 +7,26 @@ from functools import reduce
 
 # 5.3 Создайте переменную students - список объектов класса Student, с разными именами и средней оценкой.
 
-students = [Student('Andrew', 7), Student('Eugene', 4), Student('Alex', 6)]
+students = [
+    Student("Andrew", 7),
+    Student("Eugene", 4),
+    Student("Max", 10),
+    Student("Kolyan", 9),
+    Student("Alex", 6),
+]
 
 # 5.4 Посчитайте суммарную стипендию для всех студентов.
 
-scholarship_sum = reduce(lambda sum_, student: sum_ + student.get_scholarship(), students, 0)
+scholarship_sum = reduce(
+    lambda sum_, student: sum_ + student.get_scholarship(), students, 0
+)
 print(scholarship_sum)
 
 # 5.5 Посчитайте количество отличников (используйте метод is_excellent).
+
+amount_students = 0
 for student in students:
-    amount_students = 0
     if student.is_excellent():
         amount_students += 1
+
 print(amount_students)
-                
