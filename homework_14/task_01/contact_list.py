89866b0a028c38f9daf96eba8801f3560e313f08
Yauhen Zhur <evgeni1897@gmail.com>
2023-03-09 22:41:47 +0300

    add auto creation table

--- homework_14/task_01/contact_list.py
+++ homework_14/task_01/contact_list.py
@@ -3,7 +3,9 @@ import sqlite3
 
 class ContactList():
     def __init__(self):
-        self._contact_list = dict()
+        with sqlite3.connect('db.sqlite') as connection:
+            connection.execute("CREATE TABLE IF NOT EXISTS contact( id INTEGER PRIMARY KEY autoincrement, "
+                               "name varchar,number varchar)")
 
     def add_contact(self, name: str, number: str):
         with sqlite3.connect('db.sqlite') as connection:
