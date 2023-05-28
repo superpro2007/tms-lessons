4032b0e7a48b4d00b4948a18f99cf371d8d3a5c9
Yauhen Zhur <evgeni1897@gmail.com>
2023-05-28 15:54:46 +0300

    add saving to db

--- homework2_14/main.py
+++ homework2_14/main.py
@@ -1,4 +1,7 @@
-def ask_for_product():
+import psycopg2
+
+
+def ask_for_product() -> map:
     product_name = input('enter product name: \n')
     proteins = input('enter proteins amount: \n')
     fats = input('enter fats amount: \n')
@@ -11,6 +14,16 @@ def ask_for_product():
     }
     return product
 
+
+def save_to_db(product: map):
+    conn = psycopg2.connect("dbname=my_db user=my_user password=my_password host=localhost port=5432")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO products (name, proteins, fats, carbohydrates) VALUES (%s, %s, %s, %s)",
+                (product['name'], product['proteins'], product['fats'], product['carbohydrates']))
+    conn.commit()
+    cur.close()
+    conn.close()
+
 if __name__ == "__main__":
     product = ask_for_product()
-    print(product)
+    save_to_db(product)
